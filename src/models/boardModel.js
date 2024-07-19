00e1806d4d4c150581ca338d4b6852d4a2b2a98a
PhuocTri0106 <you@PhuocTri0106.com>
2024-07-19 09:35:37 +0700

    complete

--- src/models/boardModel.js
+++ src/models/boardModel.js
@@ -75,6 +75,16 @@ const pushColumnOrderIds = async (column) => {
     return result
   } catch (error) {throw new Error(error)}
 }
+const pullColumnOrderIds = async (column) => {
+  try {
+    const result = await GET_DB().collection(BOARD_COLLECTION_NAME).findOneAndUpdate(
+      { _id: new ObjectId(`${column.boardId}`) },
+      { $pull: { columnOrderIds: new ObjectId(`${column._id}`) } },
+      { returnDocument: 'after' }
+    )
+    return result
+  } catch (error) {throw new Error(error)}
+}
 const update = async (boardId, updateData) => {
   try {
     Object.keys(updateData).forEach(fieldName => {
@@ -101,5 +111,6 @@ export const boardModel = {
   findOneById,
   getDetails,
   pushColumnOrderIds,
+  pullColumnOrderIds,
   update
 }
